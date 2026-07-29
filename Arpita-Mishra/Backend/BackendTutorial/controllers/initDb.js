ce5784efb31a8a12cfa9812e84395cebea74347d
Arpita Mishra <arpitamishra30062005@gmail.com>
2026-07-29 23:03:17 +0530

    29th july get post and delete

--- Arpita-Mishra/Backend/BackendTutorial/controllers/initDb.js
+++ Arpita-Mishra/Backend/BackendTutorial/controllers/initDb.js
@@ -4,8 +4,10 @@ const initDatabase=async()=>{
     CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             username VARCHAR(50) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
             email VARCHAR(255) UNIQUE NOT NULL,
             created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+            
         );
         `
    try {
--- Arpita-Mishra/Backend/BackendTutorial/server.js
+++ Arpita-Mishra/Backend/BackendTutorial/server.js
@@ -5,7 +5,9 @@ const { initDatabase } = require('./controllers/initDb.js');
 const db = require('./models/connection.js');
 initDatabase(); 
 
-PORT=process.env.PORT;
+const PORT=process.env.PORT;
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.get('/', (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -30,9 +32,88 @@ app.get('/users', async (req, res) => {
         });
     }
 });
+
+app.post('/users', async (req, res) => {
+
+    const { username, email, password } = req.body;
+
+    try {
+        const createUserQuery = `
+            INSERT INTO users (username, email, password)
+            VALUES ($1, $2, $3)
+            RETURNING id, username, email;
+        `
+
+        const result = await db.query(createUserQuery, [
+            username,
+            email,
+            password
+        ]);
+
+        res.status(201).json({
+            status: "Success",
+            message: "Created user successfully",
+            data: result.rows[0]
+        });
+
+    } catch (error) {
+    res.json({
+        status: "Failure",
+        message: "User Cannot be created",
+        error: error
+    });
+}
+
+});
+app.delete('/users/:email', async (req, res) => {
+    const { id } = req.params;
+const deleteUserQuery = `
+        DELETE FROM users
+        WHERE id = $1;
+    `;try {
+        await db.query(deleteUserQuery, [id]);
+        res.status(200).json({
+            status: "success",
+            message: "User deleted successfully"
+ });
+    } catch (error) {
+        res.status(500).json({
+            status: "failed",
+            message: "Something went wrong"
+        });
+    }
+});
+app.post('/user', async (req, res) => {
+
+const { email, password } = req.body;
+const getUserQuery = `
+    SELECT *FROM users
+    WHERE email = $1 AND password = $2;
+    `;
+try {
+const result = await db.query(getUserQuery, [
+            email,password
+        ]);
+res.status(200).json({
+        status: "success",
+        message: "User fetched successfully",
+        data: result.rows
+        });
+
+    } 
+    catch (error) {
+
+        res.status(500).json({
+            status: "failed",
+            message: "Something is  wrong"
+        });
+
+    }
+
+});
 app.listen(PORT, (err) => {
     if (err) console.log(err);
 
     console.log(`Successfully Connected To Server at PORT: ${process.env.PORT}`);
 
-});    
+});
